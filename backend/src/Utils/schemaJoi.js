8db63209592a1ed5a59a2aa7afb035cbf04d6ad7
Carlos Felipe <carlosfelipeti@outlook.com>
2020-04-05 14:51:59 -0300

    criei validação de caracteres

---
+++ backend/src/Utils/schemaJoi.js
@@ -0,0 +1,33 @@
+const Joi = require("@hapi/joi");
+
+const Cadastro  = Joi.object({
+
+    name: Joi.string()
+        .min(3)
+        .max(20)
+        .pattern(/^[a-zA-Z\s.]{2,80}$/)
+        .required(),
+
+    nick: Joi.string()
+        .min(3)
+        .max(20)
+        .pattern(/^[a-zA-Z\s.]{2,80}$/)
+        .required(),
+    
+    password: Joi.string()
+        .min(8)
+        .max(20)
+        .pattern(/^[a-zA-Z0-9?!@#$%"&*()_-]{8,20}$/),
+
+    userLogin: Joi.string()
+        .min(3)
+        .max(20)
+        .required()
+        .pattern(/^[a-zA-Z0-9?!@#$%"&*()_-]{8,20}$/),
+    
+    number: Joi.string()
+        .min(8)
+        .max(9),
+});
+
+module.exports = Cadastro;
